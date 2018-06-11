4b2a3c4a4eddb34a62f913d6854d23bc07ee7cac
britmdykes <39972616+britmdykes@users.noreply.github.com>
2018-06-11 18:00:50 -0500

    css changes

---
+++ script.js
@@ -0,0 +1,55 @@
+// javascript
+
+
+// Player one is active
+var player='1';
+var player1color = 'red';
+var player2color = 'black';
+var column='0'
+
+// Player one selects a column (1-7)
+$('.column').on('click', function() {
+	// if column has at least 1 empty cell
+	if (column > 1) {
+		console.log('clicked')
+	} else if (column <1) {
+		console.log('Pick a new column')
+	}
+		// get location lowest empty cell
+
+		// create chip with player color 
+	$('.cell').on('click', function(){
+		$('.cell').addClass('clicked');
+	})
+		// place chip in location (1)
+
+		// look at neighbor cells and get chip color, location, and direction from source chip
+
+		// if one neighbor matches color (2)
+
+			// look at next neighbor in same direction
+
+			// if next neighbor matches color (3)
+
+				//look at next neighbor in same direction
+
+				// if next neighbor matches color (4)
+
+					// Game over player one connected four! 
+
+				// else toggle to player 2
+				var player = '2';
+
+			// else toggle to player 2
+
+		// else toggle to player 2
+
+	// else if the column has no empty cells 
+
+		// emit message to player 1 that they need to pick again 
+
+	// else if there are no available cells
+
+		// game over, cat game
+
+})
